870b6ea4f333eb76b1d6af8e9a7a1ae897360ae5
Aram Hekimian <guerrerohek121@gmail.com>
2025-01-21 11:51:06 -0600

    21/01/25 completed

---
+++ 21-01-25/challenge.js
@@ -0,0 +1,24 @@
+// Task
+
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+// Examples
+
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+const sortArray = (arr) => {
+  const odds = arr.filter((el) => el % 2 !== 0).sort((a, b) => a - b);
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      result.push(arr[i]);
+    } else {
+      result.push(odds[0]);
+      odds.shift();
+    }
+  }
+  return result;
+};
+
+sortArray([5, 8, 6, 3, 4]);
