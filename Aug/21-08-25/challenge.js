b99d4c75c55b981bc1a8fb9348c050c25f1d9836
Aram Hekimian <guerrerohek121@gmail.com>
2025-08-21 08:53:36 -0600

    21/08/25 Completed

--- Aug/21-08-25/challenge.js
+++ Aug/21-08-25/challenge.js
@@ -25,9 +25,47 @@ function areThereDuplicates(...args) {
   return false;
 }
 
-console.log(areThereDuplicates(1, 2, 3)); // false
-console.log(areThereDuplicates(1, 2, 2)); // true
-console.log(areThereDuplicates('a', 'b', 'c', 'a')); // true
-console.log(areThereDuplicates()); // false
-console.log(areThereDuplicates(true, false, true)); // true
-console.log(areThereDuplicates(null, undefined, null)); // true
+// TESTS:
+// console.log(areThereDuplicates(1, 2, 3)); // false
+// console.log(areThereDuplicates(1, 2, 2)); // true
+// console.log(areThereDuplicates('a', 'b', 'c', 'a')); // true
+// console.log(areThereDuplicates()); // false
+// console.log(areThereDuplicates(true, false, true)); // true
+// console.log(areThereDuplicates(null, undefined, null)); // true
+
+// PROBLEM 2:
+// Switch it up
+// When provided with a number between 0-9, return it in words. Note that the input is guaranteed to be within the range of 0-9.
+
+// Input: 1
+
+// Output: "One".
+
+// If your language supports it, try using a switch statement.
+
+function switchItUp(n) {
+  switch (n) {
+    case 0:
+      return 'Zero';
+    case 1:
+      return 'One';
+    case 2:
+      return 'Two';
+    case 3:
+      return 'Three';
+    case 4:
+      return 'Four';
+    case 5:
+      return 'Five';
+    case 6:
+      return 'Six';
+    case 7:
+      return 'Seven';
+    case 8:
+      return 'Eight';
+    case 9:
+      return 'Nine';
+    default:
+      return 'Not a valid number';
+  }
+}
