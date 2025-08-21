1f913d2bc0eec1104f9f64474e20d786ac53f97f
Aram Hekimian <guerrerohek121@gmail.com>
2025-08-21 09:24:59 -0600

    21/08/25 completed

--- Aug/21-08-25/challenge.js
+++ Aug/21-08-25/challenge.js
@@ -106,3 +106,35 @@ function sequenceSum(begin, end, step) {
 }
 
 // console.log(sequenceSum(1, 5, 3));
+
+// PROBLEM 5
+// Check the exam
+// The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+// The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+// If the score < 0, return 0.
+
+// For example:
+
+//     Correct answer    |    Student's answer   |   Result
+//  ---------------------|-----------------------|-----------
+//  ["a", "a", "b", "b"]   ["a", "c", "b", "d"]  →     6
+//  ["a", "a", "c", "b"]   ["a", "a", "b", "" ]  →     7
+//  ["a", "a", "b", "c"]   ["a", "a", "b", "c"]  →     16
+//  ["b", "c", "b", "a"]   ["" , "a", "a", "c"]  →     0
+
+const checkExam = (arr1, arr2) => {
+  let score = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] && arr2[i]) {
+      arr1[i] === arr2[i] ? (score += 4) : (score -= 1);
+    }
+  }
+  return score > 0 ? score : 0;
+};
+
+console.log(checkExam(['a', 'a', 'b', 'b'], ['a', 'c', 'b', 'd']));
+console.log(checkExam(['a', 'a', 'c', 'b'], ['a', 'a', 'b', '']));
+console.log(checkExam(['a', 'a', 'b', 'c'], ['a', 'a', 'b', 'c']));
+console.log(checkExam(['b', 'c', 'b', 'a'], ['', 'a', 'a', 'c']));
